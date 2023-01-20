d307eb038dfa56b00d7dcdbd01ac56096f5f7a36
Pattana Sapphanachai <60337808+6210612831@users.noreply.github.com>
2023-01-20 13:22:33 +0700

    Format some code

--- grade/views.py
+++ grade/views.py
@@ -19,29 +19,16 @@ HOST = "http://127.0.0.1:8000"
 def index(request):
     return render(request, "grade/index.html")
 
-
-def delete_table_view(request,subject):
-    try:
-        grade_list = Grade.objects.filter(subject=subject)
-        for grade in grade_list:
-            for custom_field in grade.customfield.all():
-                custom_field.delete()
-            grade.delete()
-        return HttpResponseRedirect(reverse("grade:index"))
-    except:
-        return HttpResponseRedirect(reverse("grade:index"))
     
-
+# Login page
 def login_view(request):
     try:
         if request.session['user_id']:
-            # print(request.session['user_id'])
             return render(request, "grade/grade.html")
     except:
         if request.method == "POST":
             username = request.POST["username"]
             password = request.POST["password"]
-            # response = ldap3_authen(username,password)
             data = {
                 'username': username,
                 'password': password
@@ -61,6 +48,25 @@ def login_view(request):
         return render(request, "grade/login.html")
 
 
+# Load grade data
+def get_grade_data(choice,request):
+    subject_list = Grade.objects.values_list('subject', flat=True).distinct()
+    output = []
+    for subject in subject_list:
+        if choice == 0:
+            grade_list = Grade.objects.filter(subject=subject)
+        else:
+            grade_list = Grade.objects.filter(subject=subject,std_id = request.session['user_id'])
+        header_list = ['std_id','name','subject','grade','midterm','final']
+        for grade in grade_list:
+            for custom_field in grade.customfield.all():
+                header_list.append(custom_field.name)
+            break
+        output.append({'grade_list':grade_list,'header_list':header_list})
+    return output
+
+
+# Manage_grade Page
 def manage_grade_view(request):
     try:
         request.session['user_id']
@@ -70,8 +76,6 @@ def manage_grade_view(request):
                 df = pd.read_excel(request.FILES['file'])
                 data = df.to_json(orient='records')
                 student_list = json.loads(data)
-                # print(json_str)
-                # json_str = [{"aa":11,"bb":22,"cc":33,"dd":44},{"aa":"\u0e01\u0e01","bb":"\u0e02\u0e02","cc":"\u0e04\u0e04","dd":"\u0e07\u0e07"}]
                 table_key = ['std_id','name','subject','grade','midterm','final']
                 for student in student_list:
                     student_data = {}
@@ -85,49 +89,43 @@ def manage_grade_view(request):
                     grade_obj = Grade.objects.create(std_id=student['std_id'],name=student['name'],subject=student['subject'],grade=student['grade'],midterm=student['midterm'],final=student['final'])
                     for custom_field in custom_obj_list:
                         grade_obj.customfield.add(custom_field)
-                        grade_obj.save()   
-                ### save data to excel 
-                # df = pd.DataFrame(student_list)
-                # df.to_excel("output.xlsx", index=False)
-                return HttpResponseRedirect(reverse("grade:manage_grade"))
-            except Exception as e:
-                print('File error',e)
-            return HttpResponseRedirect(reverse("grade:manage_grade"))
+                        grade_obj.save()
+                output = get_grade_data(0,request)
+                return render(request, "grade/manage_grade.html",{'output':output,'message':'upload success'})
+            except:
+                # If upload file error
+                output = get_grade_data(0,request)
+                return render(request, "grade/manage_grade.html",{'output':output,'message':'exel file error'})
         # Load data to manage_grade page
         else:
-            # subject = 'cn203'
-            subject_list = Grade.objects.values_list('subject', flat=True).distinct()
-            output = []
-            for subject in subject_list:
-                grade_list = Grade.objects.filter(subject=subject)
-                # grade_list = Grade.objects.filter(subject=subject,std_id = request.session['user_id'])
-                header_list = ['std_id','name','subject','grade','midterm','final']
-                for grade in grade_list:
-                    for custom_field in grade.customfield.all():
-                        header_list.append(custom_field.name)
-                    break
-                output.append({'grade_list':grade_list,'header_list':header_list})
-            return render(request, "grade/manage_grade.html",{'output':output})
+            output = get_grade_data(0,request)
+            return render(request, "grade/manage_grade.html",{'output':output,'message':'loaded'})
     except:
         return HttpResponseRedirect(reverse("grade:login"))
 
 
+
+def delete_table_view(request,subject):
+    try:
+        grade_list = Grade.objects.filter(subject=subject)
+        for grade in grade_list:
+            for custom_field in grade.customfield.all():
+                custom_field.delete()
+            grade.delete()
+        output = get_grade_data(0,request)
+        return render(request, "grade/manage_grade.html",{'output':output,'message':'delete success'})
+    except:
+        output = get_grade_data(0,request)
+        return render(request, "grade/manage_grade.html",{'output':output,'message':'delete error'})
+
+
+# Grade Page
 def grade_view(request):
     try:
         request.session['user_id']
         # Upload Excel file and create db to store data
         # subject = 'cn203'
-        subject_list = Grade.objects.values_list('subject', flat=True).distinct()
-        output = []
-        for subject in subject_list:
-            # grade_list = Grade.objects.filter(subject=subject)
-            grade_list = Grade.objects.filter(subject=subject,std_id = request.session['user_id'])
-            header_list = ['std_id','name','subject','grade','midterm','final']
-            for grade in grade_list:
-                for custom_field in grade.customfield.all():
-                    header_list.append(custom_field.name)
-                break
-            output.append({'grade_list':grade_list,'header_list':header_list})
+        output = get_grade_data(1,request)
         return render(request, "grade/grade.html",{'output':output})
     except:
         return HttpResponseRedirect(reverse("grade:login"))
