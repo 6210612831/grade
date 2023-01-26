67f45730adde8350273e98250617a74e1b6b3fa7
Nattanan Nawakitbamrung <6210612567@student.tu.ac.th>
2023-01-26 20:59:22 +0700

    Edit render from index.html to layout.html

--- grade/views.py
+++ grade/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import json
 import pandas as pd
-from .models import CustomGradeField,Grade
+from .models import CustomGradeField, Grade
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 import requests
@@ -16,10 +16,12 @@ from rest_framework import status
 HOST = "http://127.0.0.1:8000"
 
 # Create your views here.
-def index(request):
-    return render(request, "grade/index.html")
 
-    
+
+def index(request):
+    return render(request, "grade/layout.html")
+
+
 # Login page
 def login_view(request):
     try:
@@ -49,20 +51,22 @@ def login_view(request):
 
 
 # Load grade data
-def get_grade_data(choice,request):
+def get_grade_data(choice, request):
     subject_list = Grade.objects.values_list('subject', flat=True).distinct()
     output = []
     for subject in subject_list:
         if choice == 0:
             grade_list = Grade.objects.filter(subject=subject)
         else:
-            grade_list = Grade.objects.filter(subject=subject,std_id = request.session['user_id'])
-        header_list = ['std_id','name','subject','grade','midterm','final']
+            grade_list = Grade.objects.filter(
+                subject=subject, std_id=request.session['user_id'])
+        header_list = ['std_id', 'name',
+                       'subject', 'grade', 'midterm', 'final']
         for grade in grade_list:
             for custom_field in grade.customfield.all():
                 header_list.append(custom_field.name)
             break
-        output.append({'grade_list':grade_list,'header_list':header_list})
+        output.append({'grade_list': grade_list, 'header_list': header_list})
     return output
 
 
@@ -80,12 +84,13 @@ def manage_grade_view(request):
                 # Check if subject is exist
                 for student in student_list:
                     temp = Grade.objects.filter(subject=student['subject'])
-                    if len(temp) > 0 :
-                        output = get_grade_data(0,request)
-                        return render(request, "grade/manage_grade.html",{'output':output,'message':'subject has already exist'})
+                    if len(temp) > 0:
+                        output = get_grade_data(0, request)
+                        return render(request, "grade/manage_grade.html", {'output': output, 'message': 'subject has already exist'})
                     break
                 # Upload exel to db
-                table_key = ['std_id','name','subject','grade','midterm','final']
+                table_key = ['std_id', 'name', 'subject',
+                             'grade', 'midterm', 'final']
                 for student in student_list:
                     student_data = {}
                     custom_obj_list = []
@@ -93,39 +98,40 @@ def manage_grade_view(request):
                         if key in table_key:
                             student_data[key] = student[key]
                         else:
-                            custom_obj = CustomGradeField.objects.create(name=key,value=student[key])
+                            custom_obj = CustomGradeField.objects.create(
+                                name=key, value=student[key])
                             custom_obj_list.append(custom_obj)
-                    grade_obj = Grade.objects.create(std_id=student['std_id'],name=student['name'],subject=student['subject'],grade=student['grade'],midterm=student['midterm'],final=student['final'])
+                    grade_obj = Grade.objects.create(std_id=student['std_id'], name=student['name'], subject=student['subject'],
+                                                     grade=student['grade'], midterm=student['midterm'], final=student['final'])
                     for custom_field in custom_obj_list:
                         grade_obj.customfield.add(custom_field)
                         grade_obj.save()
-                output = get_grade_data(0,request)
-                return render(request, "grade/manage_grade.html",{'output':output,'message':'upload success'})
+                output = get_grade_data(0, request)
+                return render(request, "grade/manage_grade.html", {'output': output, 'message': 'upload success'})
             except:
                 # If upload file error
-                output = get_grade_data(0,request)
-                return render(request, "grade/manage_grade.html",{'output':output,'message':'exel file error'})
+                output = get_grade_data(0, request)
+                return render(request, "grade/manage_grade.html", {'output': output, 'message': 'exel file error'})
         # Load data to manage_grade page
         else:
-            output = get_grade_data(0,request)
-            return render(request, "grade/manage_grade.html",{'output':output,'message':'loaded'})
+            output = get_grade_data(0, request)
+            return render(request, "grade/manage_grade.html", {'output': output, 'message': 'loaded'})
     except:
         return HttpResponseRedirect(reverse("grade:login"))
 
 
-
-def delete_table_view(request,subject):
+def delete_table_view(request, subject):
     try:
         grade_list = Grade.objects.filter(subject=subject)
         for grade in grade_list:
             for custom_field in grade.customfield.all():
                 custom_field.delete()
             grade.delete()
-        output = get_grade_data(0,request)
-        return render(request, "grade/manage_grade.html",{'output':output,'message':'delete success'})
+        output = get_grade_data(0, request)
+        return render(request, "grade/manage_grade.html", {'output': output, 'message': 'delete success'})
     except:
-        output = get_grade_data(0,request)
-        return render(request, "grade/manage_grade.html",{'output':output,'message':'delete error'})
+        output = get_grade_data(0, request)
+        return render(request, "grade/manage_grade.html", {'output': output, 'message': 'delete error'})
 
 
 # Grade Page
@@ -133,7 +139,7 @@ def grade_view(request):
     try:
         request.session['user_id']
         # subject = 'cn203'
-        output = get_grade_data(1,request)
-        return render(request, "grade/grade.html",{'output':output})
+        output = get_grade_data(1, request)
+        return render(request, "grade/grade.html", {'output': output})
     except:
         return HttpResponseRedirect(reverse("grade:login"))
